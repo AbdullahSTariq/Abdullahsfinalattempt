2435f48472372f5afc8f8f7bbb30088d8131bf27
unknown <u1859779@unimail.hud.ac.uk>
2019-03-21 13:15:35 +0000

    My first Commmit

--- src/Abdullahs/Main.java
+++ src/Abdullahs/Main.java
@@ -1,34 +1,25 @@
 package Abdullahs;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.awt.event.ActionEvent;
+import javafx.scene.control.Button;
+import java.awt.*;
 import java.io.IOException;
 
 public class Main extends Application {
 
-    Stage window;
-    Scene home, venue
-
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("scene1.fxml"));
-        primaryStage.setScene(new Scene(root, 1000, 700));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Welcome to CinemaOnline");
+        primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
-
-        public void newsceneButton = 0 (ActionEvent Object event ) throw IOException{
-            Parent newRoot = FXMLLoader.load(getClass().getResource("scene1Controller.fxml"));
-            window homewindow =newsceneButton.getScene().getwindow();
-            homewindow.getScene.setRoot(newRoot);
-        }
-
-
     }
+
 
 
     public static void main(String[] args) {
--- src/Abdullahs/scene1Controller.java
+++ src/Abdullahs/scene1Controller.java
@@ -4,8 +4,9 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 
 public class scene1Controller {
-
     public Button newsceneButton;
 
 
+    public void Switchvenue(ActionEvent actionEvent) {
+    }
 }
